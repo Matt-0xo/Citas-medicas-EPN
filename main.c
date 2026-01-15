23823b273e5f973373ce49bae44f5eef2ec18166
Alancin321 <mateoaguilera9030@gmail.com>
2026-01-15 15:20:21 -0500

    Modificación visual del sistema
    Se modificó principalmente la parte visual. Primero que nada se agregó una nueva librería necesario para cambiar de color tanto como el fondo como el texto, se agregó una nueva forma de de interfaz mucho mas limpia y elegante, en donde se agregaron nuevos printf

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>   // NECESARIO para system()
 #include "usuarios.h"
 #include "admin.h"
 #include "citas.h"
@@ -8,39 +9,68 @@ int main() {
 	Usuario u;
 	int opcion;
 	
+	//configuración visual
+	//Fondo blanco, texto azul
+	//Limpiar pantalla
+	system("color F1");   
+	system("cls");        
+	
 	do {
-		printf("\n-_-_- Sistema de registro del Centro Hospitalario -_-_-\n");
-		printf("\n1. Iniciar sesion\n");
-		printf("2. Registrarse\n");
-		printf("3. Salir\n");
-		printf("\nSeleccione una opcion:\n");
+		printf("\n");
+		printf("===============================================\n");
+		printf("   SISTEMA DE REGISTRO DEL CENTRO HOSPITALARIO  \n");
+		printf("===============================================\n");
+		printf("\n");
+		printf("  1. Iniciar sesion\n");
+		printf("  2. Registrarse\n");
+		printf("  3. Salir\n");
+		printf("\n");
+		printf("-----------------------------------------------\n");
+		printf("Seleccione una opcion: ");
 		
+		// Validación de entrada numérica
 		while (scanf("%d", &opcion) != 1) {
 			while (getchar() != '\n');
-			printf("Entrada invalida. Ingrese un numero: \n");
+			printf("Entrada inválida. Ingrese un número: ");
 		}
-		
 		while (getchar() != '\n');
+		// Limpiar pantalla después de elegir opción
+		system("cls");
+		
 		
 		if (opcion == 1) {
 			if (login(&u)) {
-				if (strcmp(u.rol, "ADMIN") == 0)
+				
+				system("cls");
+				printf("Bienvenido %s\n", u.usuario);
+				printf("-----------------------------------------------\n");
+				
+				if (strcmp(u.rol, "ADMIN") == 0) {
 					menuAdmin();
-				else if (strcmp(u.rol, "PACIENTE") == 0)
+				}
+				else if (strcmp(u.rol, "PACIENTE") == 0) {
 					menuPaciente(u.idRef);
-				else if (strcmp(u.rol, "MEDICO") == 0)
+				}
+				else if (strcmp(u.rol, "MEDICO") == 0) {
 					menuMedico(u.idRef);
+				}
+				
 			} else {
 				printf("Usuario o clave incorrectos.\n");
 			}
 		}
-		
 		else if (opcion == 2) {
+			printf("\n.....REGISTRO DE USUARIO.....\n");
 			registrarUsuario();
+		}
+		else if (opcion != 3) {
+			printf("Opcion invalida. Vuelva a intentarlo.\n");
 		}
 		
 	} while (opcion != 3);
 	
-	printf("\nSaliendo del sistema...\n");
+	printf("\nGracias por usar el sistema.\n");
+	printf("Saliendo del programa...\n");
+	
 	return 0;
 }
