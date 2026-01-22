6499651f9d185ba7b12774984b81786f840160fb
Alancin321 <mateoaguilera9030@gmail.com>
2026-01-21 22:19:43 -0500

    Actualización de admin.h
    Se agregaron comentarios

--- admin.h
+++ admin.h
@@ -1,6 +1,9 @@
 #ifndef ADMIN_H
 #define ADMIN_H
-
-void menuAdmin(void);
+// --- CABECERA DEL ADMINISTRADOR ---
+// Este archivo solo le avisa al programa principal que existe un "Panel de Administrador"
+// Esta funcion es la unica que necesitamos compartir.
+// Al llamarla, inicia todo el menu de opciones del admin (Pacientes, Medicos, Citas).
+void menuAdminGeneral(void);
 
 #endif
