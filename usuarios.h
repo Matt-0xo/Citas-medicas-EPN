f23a4437a42f93fde71c461bac512a98f50504ba
Alancin321 <mateoaguilera9030@gmail.com>
2026-01-21 22:24:33 -0500

    Actualización de usuarios.h
    Se agregaron cometarios y se organizo el código.

--- usuarios.h
+++ usuarios.h
@@ -1,15 +1,25 @@
 #ifndef USUARIOS_H
 #define USUARIOS_H
 
+// --- CABECERA DE USUARIOS Y SEGURIDAD ---
+// Aqui definimos como son las cuentas para entrar al sistema.
+// Definimos la estructura de la "Cuenta".
 typedef struct {
-	char usuario[30];
-	char clave[30];
-	char rol[15];
-	char idRef[20];
+	char usuario[30];  
+	char clave[30];    
+	char rol[15];      
+	char idRef[20];   
 } Usuario;
 
+//FUNCIONES 
+
+// Funcion para pedir usuario y clave, y verificar si son correctos
 int login(Usuario *usuarioLogueado);
+
+// Guia paso a paso para crear una cuenta nueva y vincularla con un Medico o Paciente
 void registrarUsuario(void);
+
+// Revisa si el nombre de usuario ya esta ocupado para evitar duplicados
 int existeUsuario(const char *nombre);
 
 #endif
