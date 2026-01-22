a5c001faaad58398c5d02154a7d5e661b72b8837
Alancin321 <mateoaguilera9030@gmail.com>
2026-01-21 22:18:58 -0500

    Actualización de pacientes.h
    Define la estructura de datos Paciente y declara los prototipos necesarios para la manipulación de registros personales. Facilita la interacción entre el núcleo del sistema y el almacenamiento de datos, permitiendo operaciones de registro y consulta mediante interfaces limpias y bien definidas. Se agregaron comentarios dentro de todo el código.

--- pacientes.h
+++ pacientes.h
@@ -1,23 +1,34 @@
 #ifndef PACIENTES_H
 #define PACIENTES_H
 
+// --- CABECERA DE GESTION DE PACIENTES ---
+
 typedef struct {
-	char nombre[50];
-	char cedula[20];
-	int edad;
-	char telefono[20];
-	char correo[40];
+	char nombre[50];    
+	char cedula[20];    
+	int edad;           
+	char telefono[20];  
+	char correo[50];   
 } Paciente;
 
-//Funciones para el funcionamiento del modulo pacientes
-int esNumeroEnteroPositivo(const char *s);
+// FUNCIONES
 
-void menuPacientes(void);
-
+// Lee el archivo "pacientes.txt" y carga los datos en la memoria para usarlos
 void cargarPacientes(Paciente pacientes[], int *total);
+
+// Guarda todos los cambios de la memoria de vuelta al archivo de texto
 void guardarPacientes(Paciente pacientes[], int total);
 
+// Pide los datos personales y agrega un paciente nuevo a la lista
 void registrarPaciente(Paciente pacientes[], int *total);
-void consultarPacientes(Paciente pacientes[], int total);
+
+// Muestra en pantalla una lista ordenada de todos los pacientes
+int consultarPacientes(Paciente pacientes[], int total);
+
+// Permite al administrador borrar a un paciente usando su numero de cedula
+void eliminarPacienteAdmin(void);
+
+// Muestra el menu con las opciones (Registrar, Listar, Eliminar, Volver)
+void menuPacientes(void);
 
 #endif
